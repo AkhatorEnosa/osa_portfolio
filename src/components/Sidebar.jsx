adcc140641e49c5bd37978b2fe6ebbe424a499ad
Osa <aosakhogba@gmail.com>
2024-07-19 12:14:25 +0100

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,20 +12,20 @@ const Sidebar = () => {
           <li className='side-link hidden md:w-[150px] h-[60px] md:flex items-center underline px-5 md:px-5 md:pl-2'>
               Hire me <FaAngleDoubleDown />
           </li>
-          <li className='side-link md:w-[150px] h-[60px] flex items-center px-5 md:px-5 md:-translate-x-20 hover:-translate-x-0 duration-300 bg-green-700'>
+          <li className='side-link md:w-[150px] h-[60px] flex items-center px-5 md:px-5 md:-translate-x-20 hover:-translate-x-0 duration-300 bg-green-700 z-50'>
             <a href={Cv} className='w-full flex justify-between items-center'><span className="hidden md:flex">Resume</span>
                <BsFillPersonLinesFill className="text-2xl md:text-3xl"/>
             </a>
           </li>
-          <li className='side-link md:w-[150px] h-[60px] flex items-center px-5 md:px-5 md:-translate-x-20 hover:-translate-x-0 duration-300 bg-blue-900'>
+          <li className='side-link md:w-[150px] h-[60px] flex items-center px-5 md:px-5 md:-translate-x-20 hover:-translate-x-0 duration-300 bg-blue-900 z-50'>
             <a href="https://www.linkedin.com/in/akhator-osakhogba-024762139" className='w-full flex justify-between items-center'><span className="hidden md:flex">LinkedIn</span><FaLinkedin className="text-2xl md:text-3xl"/>
             </a>
           </li>
-          <li className='side-link md:w-[150px] h-[60px] flex items-center px-5 md:px-5 md:-translate-x-20 hover:-translate-x-0 duration-300 bg-black'>
+          <li className='side-link md:w-[150px] h-[60px] flex items-center px-5 md:px-5 md:-translate-x-20 hover:-translate-x-0 duration-300 bg-black z-50'>
             <a href="https://www.github.com/Akhatorenosa" className='w-full flex justify-between items-center'><span className="hidden md:flex">GitHub</span><FaGithub className="text-2xl md:text-3xl"/>
             </a>
           </li>
-          <li className='side-link md:w-[150px] h-[60px] flex items-center px-5 md:px-5 md:-translate-x-20 hover:-translate-x-0 duration-300 bg-red-600'>
+          <li className='side-link md:w-[150px] h-[60px] flex items-center px-5 md:px-5 md:-translate-x-20 hover:-translate-x-0 duration-300 bg-red-600 z-50'>
             <a href="mailto:aosakhogba@gmail.com" className='w-full flex justify-between items-center'><span className="hidden md:flex">Email</span><HiOutlineMail className="text-2xl md:text-3xl"/>
             </a>
           </li>
