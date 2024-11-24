fb6cdae900aad8d359a1356c01af661d52562695
AkhatorEnosa <osa@John-316.local>
2024-11-24 15:53:12 +0100

    updated linkedIn link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@
 const Button = (props) => {
   return (
     <a href={props.link} target="_blank" className="button cursor-pointer">
-       <button className="flex justify-center items-center px-7 py-2 text-white text-sm bg-[#003816] hover:bg-[#6aff7f] hover:text-[#003816] hover:shadow-md rounded-full transition-all duration-200">
+       <button className="flex justify-center items-center px-7 py-2 text-white text-sm bg-[#003816] hover:bg-[#6aff7f] hover:text-[#003816] hover:shadow-md rounded-full lg:border-[1px] lg:border-[#6aff7f] transition-all duration-200">
           {props.title}
       </button>
     </a>
